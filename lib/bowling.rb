19790ee8b8882e9cf75fe16a9a5dff1c67df121f
Marc Winstanley <marcwinstanley@Marcs-MacBook-Air.local>
2014-10-16 16:01:12 +0100

    Adjusted calc_strike so spares and strikes can occur in the same game
    - modified calc_strikes to use the each_at_index iterator
    - calc_strikes now just passes values to an additional array, instead of removing and adding elements to the original array

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -4,19 +4,18 @@ class Bowling
     @total = 0
     @score = []
     @spare_score = [0]
+    @strike_score = [0]
   end
+
   def frame(turn1, turn2 = nil)
     @score << turn1 << turn2
   end
 
   def calc_strikes
     @score.compact!
-    for i in 0..@score.length-1
-      if @score[i] == 10
-        puts "found a ten"
-        @score.insert(i+1, (@score[i] + @score[i + 1] + @score[i + 2]))
-        @score.delete_at(i)
-        
+    @score.each_with_index do |element, index|
+      if element == 10
+        @strike_score << @score[index + 1] << @score[index + 2]
       end
     end
   end
@@ -32,7 +31,6 @@ class Bowling
   def calc_score
     calc_spares
     calc_strikes
-    # puts @spare_score
-    @total = @score.inject(:+) + @spare_score.inject(:+)
+    @total = @score.inject(:+) + @spare_score.inject(:+) + @strike_score.inject(:+)
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe "Bowling Kata" do
   it "checks that spares and strikes are calculated together" do
     game = Bowling.new
     game.frame(1, 9)
-    game.frame(10, 0)
+    game.frame(10)
     game.frame(5, 3)
     expect(game.calc_score).to eq(46)
   end
